fa4b483816052806df268ad7bc08fe65bf405829
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-09 12:22:18 +0530

    practice

---
+++ Task/basics.js
@@ -0,0 +1,15 @@
+// <!-- 1.var -->
+
+var name = "shraddha";
+console.log("var first value:", name);
+
+
+// Update //
+name = "gunjkar";
+console.log("var updated value:", name);
+
+// redeclare //
+var name = "Developer";
+console.log("var redeclared value:", name);
+
+
