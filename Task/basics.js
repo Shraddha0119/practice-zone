90e91ebdedc85b0d4622b4e9441b4606caa22551
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-09 12:31:08 +0530

    practice

--- Task/basics.js
+++ Task/basics.js
@@ -13,3 +13,27 @@ var name = "Developer";
 console.log("var redeclared value:", name);
 
 
+// 2. let
+
+let city = "mumbai";
+console.log("let first value:", city);
+
+// update value
+city = "pune";
+console.log("let updated value:", city);
+
+// let city = "delhi"(redclare) gives error
+
+
+// 3.const
+
+const country = "India";
+console.log("const first value:", country);
+
+// updated value
+// country="usa";
+// console.log("const updated value:", country);gives error
+
+// const country = "canada"(redclare) gives error
+
+
