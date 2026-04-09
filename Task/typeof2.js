3f8441190fdd0960aabceec825b29475d5fb1955
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-09 15:52:40 +0530

    practice

---
+++ Task/typeof2.js
@@ -0,0 +1,32 @@
+// 1 string
+
+let name = "shraddha";
+console.log(name);
+console.log(typeof name);
+
+// number
+let age = 21;
+console.log(age);
+console.log(typeof age)
+
+// decimal number
+
+let price = 99.99;
+console.log(price);
+console.log(typeof price);
+
+// boolean
+let isStudent = true;
+console.log(isStudent);
+console.log( typeof isStudent)
+
+// null
+// let date = null;
+// console.log(data);
+// console.log(typeof data)
+
+// Array
+ let colors = ["red","yellow","blue"]
+ console.log(colors);
+ console.log(typeof colors);
+ 
