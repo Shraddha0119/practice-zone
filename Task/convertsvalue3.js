b2a829f5a89f3b7158f8950fe23d7a2e9355ffa5
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-09 18:26:17 +0530

    practice

---
+++ Task/convertsvalue3.js
@@ -0,0 +1,58 @@
+// step 1: string to number
+
+// let student ="40";
+
+// console.log(student);
+// console.log(typeof student);
+
+// let  convertNumber = Number(student);
+// console.log(convertNumber);
+// console.log(typeof convertNumber);
+
+// step2: number to string
+
+// let marks = 90;
+// console.log(marks);
+// console.log(typeof marks);
+
+// let convertstring = String(marks);
+// console.log(convertstring);
+// console.log(typeof convertstring);
+
+// step3:number to boolean
+
+// let value1 =1;
+// let value2=0;
+
+// console.log(Boolean(value1));
+// console.log(Boolean(value2));
+
+// 4.step4: string to boolean
+
+// let car1 = "hello";
+// let car2 = "";
+
+// console.log(Boolean(car1));
+// console.log(Boolean(car2));
+
+// step5:boolean to string
+
+let answer = true;
+console.log(answer);
+console.log(typeof answer);
+
+let quetion = String(answer);
+console.log(quetion);
+console.log(typeof quetion);
+
+
+
+
+
+
+
+
+
+
+
+
