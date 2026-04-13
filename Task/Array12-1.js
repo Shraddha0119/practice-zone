a93b6205dddb472c103332675516600fe8f8b216
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-13 19:00:24 +0530

    practice

---
+++ Task/Array12-1.js
@@ -0,0 +1,30 @@
+// <!-- Level 5: Arrays 
+// Task 12: Perform push, pop, shift, unshift. 
+// Objective 
+// Learn how to: 
+//  Add/remove elements from array  
+//  Understand array mutation (original array changes)  
+// Approach (How to Think) 
+// 1. Start with a simple array  
+// 2. Perform operations one by one  
+// 3. Print after each step  -->
+
+let arr = ["mango","banana","orange"];
+console.log("initial array:", arr);
+
+
+arr.push("graps");
+console.log("frute add:", arr);
+
+arr.pop("graps");
+console.log("fruit remove", arr);
+
+arr.shift();
+console.log("fruits shift:",arr);
+
+arr.unshift("mango");
+console.log("fruits unshift:",arr);
+
+
+
+
