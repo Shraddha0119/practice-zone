2d2e6565cefb963e2d153c64772b08eb4ebdf6df
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-18 14:36:42 +0530

    practice

---
+++ Task/Advance20.js
@@ -0,0 +1,42 @@
+// Task 20: Hoisting test with var, let, const. 
+// Objective 
+// Understand: 
+//  What is hoisting  
+//  Difference between var, let, const  
+//  Temporal Dead Zone (TDZ)  
+// Approach (How to Think) 
+// 1. Ask:  
+// o “What happens if I use a variable before declaring it?”  
+// 2. Try with:  
+// o var  
+// o let  
+// o const  
+// 3. Observe Behaviour
+
+
+// ANSWER:
+
+// var//
+// console.log(a)   (undifines)
+// var a = 9;
+// console.log(a);  
+
+
+// lett//
+// console.log(b);                     refferance error
+
+let b = 10;
+// console.log(b);10
+
+
+// const //
+// console.log(a);         Refferece error
+
+const a = 90;
+// console.log(a);    //90
+
+
+
+
+
+
---
+++ Task/Function19-3.js
@@ -0,0 +1,23 @@
+// : Convert to arrow function. 
+// Objective 
+// Learn how to: 
+//  Convert normal function → arrow function  
+//  Write cleaner code  
+// Approach (How to Think) 
+// 1. Remove function keyword  
+// 2. Add =>  
+// 3. If single return → shorten
+
+
+let add = (a,b) => a+b;
+
+console.log("\n arrowfunction")
+console.log(add);
+
+
+console.log(add(3,5) );
+
+
+
+
+
